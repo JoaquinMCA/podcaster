4cd0cb082005926aa0c2b80dcb8d97355388c02d
Joaquin Garcia <joaquingm@MBP-de-Joaquin.home>
2023-03-16 22:09:42 +0100

    e2e test with cypress added

---
+++ cypress/e2e/app-navigation.cy.js
@@ -0,0 +1,20 @@
+describe("Complete app navigation", () => {
+  it("Navigate through the app", () => {
+    cy.visit("http://localhost:3000/");
+    // Check if main header link is present
+    cy.get("#root > h1 > a").contains("Podcaster");
+
+    // Navigate to the first podcast
+    cy.get("#root > main > ul > li:nth-child(1) > a > div").click();
+    // Check if podcast details card is rendered:
+    cy.get("div").contains("Description:");
+
+    // Select the first episode
+    cy.get("table > tbody > tr:nth-child(1) > td:nth-child(1) > a").click();
+    // Check if podcast details card is still on view:
+    cy.get("div").contains("Description:");
+
+    // Check if audio player is visible:
+    cy.get("audio");
+  });
+});
